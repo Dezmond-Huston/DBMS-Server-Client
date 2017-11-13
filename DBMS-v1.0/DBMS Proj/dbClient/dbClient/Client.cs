d6f88b764639d47eda96c7c6039a812448ad3e35
Dezmond-Huston <Dez.Huston@gmail.com>
2017-11-13 10:56:08 -0600

    adding unzipped DBMS folder

---
+++ DBMS-v1.0/DBMS Proj/dbClient/dbClient/Client.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Net.Sockets;
+
+namespace dbClient
+{
+    public class Client
+    {
+        private static string SRSU_IP = GetIP.returnCheckCurrentIP();
+
+        public static void Main()
+        {
+            
+            int port = 8001;
+
+            while (!connect(port))
+            {
+                port++;
+                Console.WriteLine("Trying port " + port);
+                if (port > 8011) port = 8001;
+            }
+        }
+
+        public static string toString(byte[] data, int size)
+        {
+            string recieved = "";
+            for (int i = 0; i < size; i++)
+            {
+                recieved = recieved + Convert.ToChar(data[i]);
+            }
+            return recieved;
+        }
+
+        private static void send(Stream stream, ASCIIEncoding asen, string message)
+        {
+
+            byte[] baAnswer = asen.GetBytes(message);
+            stream.Write(baAnswer, 0, baAnswer.Length);
+        }
+
+        private static string getData(Stream stream, int size)
+        {
+            byte[] bb = new byte[size];
+            int k = stream.Read(bb, 0, size);
+            return toString(bb, k);
+        }
+
+
+        private static string sendAndRecieve(Stream stream, ASCIIEncoding asen, string message, int size)
+        {
+            send(stream, asen, message);
+            return getData(stream, size);
+        }
+
+        static int FreeTcpPort()
+        {
+            TcpListener l = new TcpListener(IPAddress.Loopback, 8002);
+            l.Start();
+            int port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }
+
+        public static bool connect(int port)
+        {
+            try
+            {
+                Console.WriteLine("1. Connecting ...");
+                TcpClient tcpclnt = new TcpClient();
+
+                //tcpclnt.Connect(SRSU_IP, port);
+                tcpclnt.Connect(SRSU_IP, FreeTcpPort());
+                Stream stream = tcpclnt.GetStream();
+                ASCIIEncoding asen = new ASCIIEncoding();
+
+                Console.Clear();
+
+                Console.Write(sendAndRecieve(stream, asen, "start", 256));
+
+                while (true)
+                {
+                    string userResponse = Console.ReadLine();
+                    if (userResponse.ToLower() == "quit" || userResponse.ToLower() == "exit")
+                    {
+                        send(stream, asen, userResponse.ToLower());
+                        break;
+                    }
+                    else if (userResponse == "clear")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Enter a command or 'help' to bring up the list of commands.");
+                        Console.Write("db:> ");
+                    }
+                    //handles when the user just presses enter without entering anything
+                    else if (userResponse == "")
+                    {
+                        Console.Write("db:> ");
+                    }
+                    else
+                    {////////////////////////user issued command
+                        string networkResponse = sendAndRecieve(stream, asen, userResponse, 1024);
+
+                        if (networkResponse.StartsWith("clear"))
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Enter a command or 'help' to bring up the list of commands.");
+                            Console.Write("db:> ");
+                        }
+                        //isn't normally called, but included just in case
+                        else if (networkResponse == "")
+                        {
+                            Console.Write("db:> ");
+                        }
+                        else
+                        {
+                            Console.Write(networkResponse);
+                        }
+                    }
+                }
+                tcpclnt.Close();
+                return true;
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+                return false;
+            }
+        }
+
+    }
+}
---
+++ DBMS-v1.0/DBMS Proj/dbClient/dbClient/GetMyIP.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Linq;
+using System.ComponentModel;
+
+namespace dbClient
+{
+    public class GetIP
+    {
+        public static string[] LocalIP { get; private set; }
+
+        public GetIP()
+        {
+        }
+
+        public static string CheckCurrentIP(NetworkInterfaceType networkType)
+        {
+  
+
+            string userIPV4 = "";
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == networkType && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            userIPV4 = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return userIPV4;
+        }
+
+        public static NetworkInterfaceType getUserNetworkType()
+        {
+            if (isOnline())
+            {
+                try
+                {
+                    var checkConnectionProcess = new Process
+                    {
+                        StartInfo =
+                          {
+                              FileName = "netsh.exe",
+                              Arguments = "wlan show interfaces",
+                              UseShellExecute = false,
+                              RedirectStandardOutput = true,
+                              CreateNoWindow = true
+                          }
+                    };
+                    checkConnectionProcess.Start();
+
+                    var output = checkConnectionProcess.StandardOutput.ReadToEnd();
+                    var line = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                                     .FirstOrDefault(l => l.Contains("SSID") && !l.Contains("BSSID"));
+                    if (line == null)
+                    {
+                        return NetworkInterfaceType.Unknown;
+                    }
+                    var ssid = line.Split(new[] { ":" }, StringSplitOptions.RemoveEmptyEntries)[1].TrimStart();
+                    return NetworkInterfaceType.Wireless80211;
+                }
+                catch (Win32Exception e)
+                {
+                    Console.WriteLine("Not connected to Wifi: " + e.StackTrace);
+                    return NetworkInterfaceType.Ethernet;
+                }
+            }
+
+            return NetworkInterfaceType.Unknown;
+        }
+
+        public static bool isOnline()
+        {
+
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "8.8.8.8";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            catch (Exception e)
+            {
+                Console.Write("Connection check error: " + e.StackTrace);
+                return false;
+            }
+
+
+        }
+
+        public static string returnCheckCurrentIP()
+        {
+            return (CheckCurrentIP(getUserNetworkType()));
+        }
+
+    }
+}
